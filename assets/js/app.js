d513d8fa2bff3513300783f4376f1df7844892ba
taiwo oluwole-young <taiwooluwoleyoung@gmail.com>
2023-03-13 18:47:15 +0100

    Edited the projects-section

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,10 @@ const toggleBtn = document.querySelector('.togglebtn');
 const closeBtn = document.querySelector('.closebtn');
 const sideBar = document.querySelector('.side-bar');
 const topSection = document.querySelector('.top');
+const container = document.querySelector('.container');
+// Animation functions
 
+// Button functions
 toggleBtn.addEventListener('click', function(){
     sideBar.classList.toggle('toggle');
     topSection.classList.toggle('top-block');
