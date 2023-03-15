6f47cfd4a0ebb0c754bd55646c73303bb79fdf50
taiwo oluwole-young <taiwooluwoleyoung@gmail.com>
2023-03-15 11:35:28 +0100

    Added animations on scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,24 @@ const sideBar = document.querySelector('.side-bar');
 const topSection = document.querySelector('.top');
 const container = document.querySelector('.container');
 // Animation functions
+function animateOnScroll() {
+  const elements = document.querySelectorAll('.fade-in');
+  elements.forEach((element) => {
+    const elementTop = element.getBoundingClientRect().top;
+    const elementBottom = element.getBoundingClientRect().bottom;
+    const viewportHeight = window.innerHeight;
+    
+    if (elementTop < viewportHeight && elementBottom >= 0) {
+      element.classList.add('active');
+    } else {
+      element.classList.remove('active');
+    }
+  });
+}
 
+window.addEventListener('scroll', animateOnScroll);
+
+window.addEventListener('load', animateOnScroll);
 // Button functions
 toggleBtn.addEventListener('click', function(){
     sideBar.classList.toggle('toggle');
